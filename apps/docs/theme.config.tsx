2a85fa24dfa6380efaf295521c1156f543795397
PiotrekPKP <peterpkppeter@gmail.com>
2023-04-07 12:36:17 +0200

    feat: docs base

--- apps/docs/theme.config.tsx
+++ apps/docs/theme.config.tsx
@@ -1,18 +1,16 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Create Taux App</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: "https://github.com/PiotrekPKP/create-taux-app",
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase:
+    "https://github.com/PiotrekPKP/create-taux-app/tree/main/apps/docs",
   footer: {
-    text: 'Nextra Docs Template',
+    text: "Create Taux App",
   },
-}
+};
 
-export default config
+export default config;
