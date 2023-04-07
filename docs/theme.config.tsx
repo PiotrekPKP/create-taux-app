c4558f7624f942ad70ac8bc83bae73cb44c06f09
PiotrekPKP <peterpkppeter@gmail.com>
2023-04-07 19:28:42 +0200

    feat: docs update

--- docs/theme.config.tsx
+++ docs/theme.config.tsx
@@ -1,8 +1,14 @@
-import React from "react";
+import Head from "next/head";
+import Image from "next/image";
 import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>Create Taux App</span>,
+  logo: (
+    <span style={{ display: "flex", alignItems: "center", gap: "13px" }}>
+      <Image alt="" src="/logo.svg" width={30} height={30} />
+      <span>Create Taux App</span>
+    </span>
+  ),
   project: {
     link: "https://github.com/PiotrekPKP/create-taux-app",
   },
@@ -11,6 +17,9 @@ const config: DocsThemeConfig = {
   footer: {
     text: "Create Taux App",
   },
+  useNextSeoProps: () => ({
+    titleTemplate: "%s - Create Taux App",
+  }),
 };
 
 export default config;
